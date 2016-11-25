894f44385d85b54e77778de77a30cd047a17c2ac
Amir Eldor <amir@eize.ninja>
2016-11-26 01:14:28 +0200

    Add eslint to webpack build

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 const animals = {'him': 12, 'herzl': 4, 'denmark': -3};
 let {herzl} = animals;
 
-document.write("hi " + herzl);
+document.write('hi ' + herzl);
--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, '../src');
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -5,14 +9,23 @@ module.exports = {
     filename: 'crocjaws.js'
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        include: [srcPath, path.resolve(__dirname, '../tools')]
+      },
+    ],
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        include: srcPath
       },
       {
         test: /\.styl$/,
-        loader: 'stylus-loader'
+        loader: 'stylus-loader',
+        include: srcPath
       }
     ]
   }

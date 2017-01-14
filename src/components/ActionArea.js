98a55dc8e4c2b4bc1660318b8452d8fa726a17df
Amir Eldor <amir@eize.ninja>
2017-01-14 21:28:24 +0200

    Action area html mockup

--- src/components/ActionArea.js
+++ src/components/ActionArea.js
@@ -6,7 +6,18 @@ export default {
 
   view: function (ctrl, args) {
     return m('div', [
-      m('h2', "action AREA")
+      m('div.submit', [
+        m('h3', "feed the croc"),
+        m('button', "Feed the croc!")
+      ]),
+      m('div.meals', [
+        m('h3', "your latest meals"),
+        m('ul', [
+          m('li', m('a', { href: 'https://crck.fr/temp-temp'}, 'crck.fr/mighty-bison')),
+          m('li', m('a', { href: 'https://crck.fr/temp-temp'}, 'crck.fr/ajar-bat')),
+          m('li', m('a', { href: 'https://crck.fr/temp-temp'}, 'crck.fr/heoric-cockroach')),
+        ])
+      ])
     ])
   }
 }

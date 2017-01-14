b5bb467dad92e9e4a9c5f6a2bd5e8e3cecd14e46
Amir Eldor <amir@eize.ninja>
2017-01-14 17:06:58 +0200

    Demonstrating mithril doing magic after page is loaded

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,29 @@
 import m from 'mithril'
 
+const number = m.prop(100)
+
 export default {
   controller: function (args) {
+    this.number = m.prop(100)
+    this.magic = () => {
+      const newNumber = Math.ceil(Math.random() * 5000)
+      console.log('ting!', newNumber)
+      number(newNumber)
+      console.log('num', number())
+    }
+
+    if (typeof window !== 'undefined') {
+      m.startComputation()
+      setTimeout(() => { this.magic(); m.endComputation() }, 777)
+    }
   },
   view: function (ctrl, args) {
-    return m('h1', "Monkey island")
+    return m('div.app', [
+      m('h1', "Monkey island"),
+      m('span', [
+        "Your lucky number is ",
+        m('strong', number())
+      ])
+    ])
   }
 }
